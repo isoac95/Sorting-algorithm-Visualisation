795804c28491b52b4c0813a809f9139bc5863846
Ante Culo <anteculoaa@gmail.com>
2020-06-15 16:16:57 +0200

    MergeSort added

--- sortalg.py
+++ sortalg.py
@@ -23,7 +23,7 @@ class App:
     def create_ui(self):
         self.reset_button = tk.Button(self.root, text="Reset", command=lambda: self.reset())
         self.play_button = tk.Button(self.root, text="Play", command=lambda: self.play())
-        self.drop_menu = tk.OptionMenu(self.root, self.alg, "BubbleSort", "HeapSort", "QuickSort")
+        self.drop_menu = tk.OptionMenu(self.root, self.alg, "BubbleSort", "HeapSort", "MergeSort", "QuickSort")
         self.label = tk.Label(self.root, text="Choose the sorting alogrithm", bg="RoyalBlue2")
         self.drop_menu.grid(row=0, column=0, pady=10)
         self.play_button.grid(row=0, column=1)
@@ -51,12 +51,15 @@ class App:
         if self.alg.get() == "BubbleSort":
             self.bubble_sort()
             self.label.configure(text="BubbleSorted! ;)")
-        elif self.alg.get() == "QuickSort":
-            self.quick_sort(chr(0+65), chr(self.bars - 1 + 65))
-            self.label.configure(text="QuickSorted! ;)")
         elif self.alg.get() == "HeapSort":
             self.heap_sort()
             self.label.configure(text="HeapSorted! ;)")
+        elif self.alg.get() == "MergeSort":
+            self.merge_sort([chr(c+65) for c in range(20)])
+            self.label.configure(text="MergeSorted! ;)")
+        elif self.alg.get() == "QuickSort":
+            self.quick_sort(chr(0+65), chr(self.bars - 1 + 65))
+            self.label.configure(text="QuickSorted! ;)")
         self.reset_button.configure(state="active")
         self.play_button.configure(state="active")
         self.drop_menu.configure(state="active")
@@ -228,6 +231,97 @@ class App:
             self.canvas.itemconfig(self.canvas.find_withtag("tag2"), tag=tgi)
 
             self.heapify(n, ord(largest)-65)
+
+    def merge_sort(self, arr):
+        self.label.configure(text="MergeSorting")
+        if len(arr) > 1:
+            mid = len(arr) // 2
+            L = arr[:mid]
+            R = arr[mid:]
+
+            self.merge_sort(L)
+            self.merge_sort(R)
+            i = j = k = 0
+            dist = xdistance = self.canvas.coords(self.canvas.find_withtag(L[0]))[0]
+            while i < len(L) and j < len(R):
+                bheightl = self.height - self.canvas.coords(self.canvas.find_withtag(L[i]))[1]
+                bheightr = self.height - self.canvas.coords(self.canvas.find_withtag(R[j]))[1]
+                if bheightl < bheightr:
+                    arr[k] = L[i]
+                    self.canvas.itemconfig(self.canvas.find_withtag(L[i]), fill="orange")
+                    xdistance = self.canvas.coords(self.canvas.find_withtag(L[i]))[0] - dist
+                    for c in range(int(abs(xdistance))):
+                        if xdistance > 0:
+                            self.canvas.move(self.canvas.find_withtag(L[i]), -1, 0)
+                            time.sleep(0.01)
+                            self.canvas.update()
+                        elif xdistance < 0:
+                            self.canvas.move(self.canvas.find_withtag(L[i]), 1, 0)
+                            time.sleep(0.01)
+                            self.canvas.update()
+                        else:
+                            break
+                    self.canvas.itemconfig(self.canvas.find_withtag(L[i]), fill="DodgerBlue3")
+                    i += 1
+                else:
+                    arr[k] = R[j]
+                    self.canvas.itemconfig(self.canvas.find_withtag(R[j]), fill="orange")
+                    xdistance = self.canvas.coords(self.canvas.find_withtag(R[j]))[0] - dist
+                    for c in range(int(abs(xdistance))):
+                        if xdistance > 0:
+                            self.canvas.move(self.canvas.find_withtag(R[j]), -1, 0)
+                            time.sleep(0.01)
+                            self.canvas.update()
+                        elif xdistance < 0:
+                            self.canvas.move(self.canvas.find_withtag(R[j]), 1, 0)
+                            time.sleep(0.01)
+                            self.canvas.update()
+                        else:
+                            break
+                    self.canvas.itemconfig(self.canvas.find_withtag(R[j]), fill="DodgerBlue3")
+                    j += 1
+                k += 1
+                dist += 25
+
+            while i < len(L):
+                arr[k] = L[i]
+                self.canvas.itemconfig(self.canvas.find_withtag(L[i]), fill="orange")
+                xdistance = self.canvas.coords(self.canvas.find_withtag(L[i]))[0] - dist
+                for c in range(int(abs(xdistance))):
+                    if xdistance > 0:
+                        self.canvas.move(self.canvas.find_withtag(L[i]), -1, 0)
+                        time.sleep(0.01)
+                        self.canvas.update()
+                    elif xdistance < 0:
+                        self.canvas.move(self.canvas.find_withtag(L[i]), 1, 0)
+                        time.sleep(0.01)
+                        self.canvas.update()
+                    else:
+                        break
+                self.canvas.itemconfig(self.canvas.find_withtag(L[i]), fill="DodgerBlue3")
+                i += 1
+                k += 1
+                dist += 25
+
+            while j < len(R):
+                arr[k] = R[j]
+                self.canvas.itemconfig(self.canvas.find_withtag(R[j]), fill="orange")
+                xdistance = self.canvas.coords(self.canvas.find_withtag(R[j]))[0] - dist
+                for c in range(int(abs(xdistance))):
+                    if xdistance > 0:
+                        self.canvas.move(self.canvas.find_withtag(R[j]), -1, 0)
+                        time.sleep(0.01)
+                        self.canvas.update()
+                    elif xdistance < 0:
+                        self.canvas.move(self.canvas.find_withtag(R[j]), 1, 0)
+                        time.sleep(0.01)
+                        self.canvas.update()
+                    else:
+                        break
+                self.canvas.itemconfig(self.canvas.find_withtag(R[j]), fill="DodgerBlue3")
+                j += 1
+                k += 1
+                dist += 25
 
 
 if __name__ == '__main__':
